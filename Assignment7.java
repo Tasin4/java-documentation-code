81093e757a33a8da8e01f5c59665893acf5af097
Tasin4 <mustakthamidtasin9@gmail.com>
2024-09-15 21:44:24 +0600

    Update Assignment7.java

--- Assignment7.java
+++ Assignment7.java
@@ -5,6 +5,21 @@
 // step 4: if user input n / N then print you are not a java lover
 public class Assignment7 {
   public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+        char ch;
+        System.out.print("Do you love java?: ");
+        ch = input.next().charAt(0);
+      if(ch=='y' || ch=='Y'){ //n please input y/Y or n/N for your output
+     System.out.println("You are a java lover");
+        
+    }
     
-  }
+     else if 
+         (ch=='n' ||  ch=='N'){ 
+    System.out.println("you are  not a java lover");
+    }else{
+         System.err.println("for output press y/Y,n/N");
+     }
 }
+}
+    
